47a4bc6161df9ab11897992fa14e0da39b2e4f6d
Jeremy Ross <jeremypross@gmail.com>
2020-01-17 14:55:45 -0500

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(authRoutes);
+
+
+
+app.listen(3000, () => {
+    console.log('Listening on Port 3000');
+});
---
+++ server/src/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// pre save hook - function that will run before we attempt to save an instance of user within our DB
+// need to use keyword function here to get access to this within function scope
+userSchema.pre('save', function(next) {
+    const user = this;
+    // if user hasn't modifed password
+    if (!user.isModified('password')) {
+        // move on
+        return next();
+    }
+
+    // else salt password w bcrypt
+    bcrypt.genSalt('10', (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            // replace plaintext password with salted and hashed password
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// automate password checking process if user is logging in
+// need to use keyword function again here to get access to this within function scope (arrow function would assign this to context of this file)
+// candidatePassword is what user is trying to login with
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+
+    // pass promise callback function that is invoked with 2 arguments: resolve & reject
+    
+    return new Promise ((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err);
+            }
+            if (!isMatch) {
+                return reject(false);
+            }
+            resolve(true);
+        });
+    });
+};
+
+mongoose.model('User', userSchema);
