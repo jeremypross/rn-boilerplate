e5ba58c49a9ee6414b3a87799decfe1fb9779efb
Jeremy Ross <jeremypross@gmail.com>
2020-01-19 12:27:29 -0500

    added mongo logic to index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,26 @@ app.use(bodyParser.json());
 
 app.use(authRoutes);
 
+const mongoUri = 'mongodb+srv://jeremyross.dev:w1epqkgf@cluster0-qeayx.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    userCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB instance!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${res.user.email}.`);
+});
+
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
 });
